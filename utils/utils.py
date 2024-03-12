db1337cc61d1e7ae93453023683e107fd1179f9d
Filip Haužvic <hauzvicfilip@gmail.com>
2024-03-12 20:21:59 +0100

    Display mask on top of original image

--- utils/utils.py
+++ utils/utils.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 
 
 def plot_img_and_mask(img, mask):
-    classes = mask.max() + 1
-    fig, ax = plt.subplots(1, classes + 1)
-    ax[0].set_title('Input image')
-    ax[0].imshow(img)
-    for i in range(classes):
-        ax[i + 1].set_title(f'Mask (class {i + 1})')
-        ax[i + 1].imshow(mask == i)
+    # classes = mask.max() + 1
+    # fig, ax = plt.subplots(1, classes + 1)
+    # ax[0].set_title('Input image')
+    # ax[0].imshow(img)
+    # ax[0].imshow(mask, alpha=0.5)
+    # for i in range(classes):
+    #     ax[i + 1].set_title(f'Mask (class {i + 1})')
+    #     ax[i + 1].imshow(mask == i)
+    
+    plt.imshow(img)
+    plt.imshow(mask, alpha=0.5)
+
     plt.xticks([]), plt.yticks([])
     plt.show()
